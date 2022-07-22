a8ed1298d2acd3927d1e7be753c9c9040127c6a2
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-07-23 02:21:58 +0530

    req

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
+const { text } = require('express');
 const express = require('express')
 const app = express();
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const cron = require('node-cron');
+const { v3, v1 } = require('uuid');
 
 // ...
-
+app.use(express.json())
 
 cron.schedule("*/2 * * * * *", async function() {
     console.log("hey");
     time = new Date().getTime()
     const status = await (await fetch('https://blr1.blynk.cloud/external/api/isHardwareConnected?token=IVVXZWTMfbZL3hjpxkeZO1OLyWrBQTne')).text()
     console.log(status);
-    if(status)
+    if(status === "true")
     {
 
         console.log("LanGuard Connected");
         console.log("fetching..."); 
         fetch('https://blr1.blynk.cloud/external/api/get?token=IVVXZWTMfbZL3hjpxkeZO1OLyWrBQTne&v0&v1&v2&v3')
-            .then(res => res.text())
-            .then(text => console.log(text));
+            .then(result => result.json())
+            .then((text,res) => {
+               res.status(200).json({
+                text:text
+               })
+            });
     }
     else {
         console.log("Languard is offline");
+    }
+
+});
+
+app.get('/',async(req,res)=>{
+    const status = await (await fetch('https://blr1.blynk.cloud/external/api/isHardwareConnected?token=IVVXZWTMfbZL3hjpxkeZO1OLyWrBQTne')).text()
+    console.log(status);
+    if(status === "true")
+    {
+
+        console.log("LanGuard Connected");
+        console.log("fetching..."); 
+        fetch('https://blr1.blynk.cloud/external/api/get?token=IVVXZWTMfbZL3hjpxkeZO1OLyWrBQTne&v0&v1&v2&v3')
+            .then(result => result.json())
+            .then((text,res) => {
+               res.status(200).json({
+                text:text
+               })
+            });
+    }
+    else {
+        error = {message : "Languard is offline"}
+        res.json({message:error});
     }
 });
 
@@ -31,7 +60,7 @@ const calculateRisk = (data) => {
         console.log("ALERT ALERT ! LANDSLIDE POSSIBILITY!")
     }
 }
-
+console.log("this is sample"+v3);
 
 
 
