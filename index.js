d29d824fd9dce1dd71b9825eddfdbb982fed5a5f
Karthik-Sunil-K <karthiksunil038@gmail.com>
2022-07-23 02:57:57 +0530

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 const whitelist = [
     "http://localhost",
-    "http://localhost:5500",
+    "http://127.0.0.1:5500",
   ];
   const corsOptions = {
     origin: function (origin, callback) {
